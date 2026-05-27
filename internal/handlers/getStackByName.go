43ef71955c67c4f4edc7cc41c07ebe484838cc9b
Jairo Humberto Gomez Castillo <jahugomez@gmail.com>
2026-05-26 22:36:54 -0500

    refactor stack handling and error management

--- internal/handlers/getStackByName.go
+++ internal/handlers/getStackByName.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"encoding/json"
 	"errors"
+	"go-backend-training/internal/stack"
 	"net/http"
 )
 
@@ -11,18 +12,17 @@ func (h *Handler) SearchStackByNameHandler(w http.ResponseWriter, r *http.Reques
 	stackName := r.PathValue("stackName")
 
 	resp, err := h.uc.GetStackByName(r.Context(), applicationName, stackName)
+	if errors.Is(err, stack.ErrStackNotFound) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
 	if err != nil {
-		if errors.Is(err, err) {
-			http.Error(w, errors2.ErrStackNotFound.Error(), http.StatusNotFound)
-			return
-		}
-		http.Error(w, errors2.ErrSearchStackByName.Error(), http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(resp); err != nil {
-		http.Error(w, "failed to encode response", http.StatusInternalServerError)
-		return
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
